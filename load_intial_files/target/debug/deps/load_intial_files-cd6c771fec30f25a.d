25dfc36974788307c2bdef3161690b757e0b2b76
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2018-04-27 17:05:08 -0400

    reading file names

---
+++ load_intial_files/target/debug/deps/load_intial_files-cd6c771fec30f25a.d
@@ -0,0 +1,5 @@
+C:\Workspace\rust\stock-market-data-warehouse\load_intial_files\target\debug\deps\load_intial_files-cd6c771fec30f25a.exe: src\main.rs
+
+C:\Workspace\rust\stock-market-data-warehouse\load_intial_files\target\debug\deps\load_intial_files-cd6c771fec30f25a.d: src\main.rs
+
+src\main.rs:
