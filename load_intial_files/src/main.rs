457b5fd17a497bf2a06d58fd65eac493cb72437b
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2018-05-07 22:44:38 -0400

    symbols file

--- load_intial_files/src/main.rs
+++ load_intial_files/src/main.rs
@@ -1,68 +1,15 @@
 use std::fs;
-use std::fs::OpenOptions;
-use std::io::prelude::*;
-use std::path::Path;
-use std::fs::File;
-use std::error::Error;
+use std::io::Write;
 
 fn main() {
-    let files_location = String::from("../input_data/stocks/Test/");
-    let v = get_names_of_symbol_files(&files_location);
-    let v2 = v.clone();
-    //for i in v {
-    //    let symbol = extract_symbol(i);
-    //    println!("{}", symbol);
-    //}
-
-    let output_base_location = String::from("../output_data/stocks/");
-    let symbols_file_name = String::from("symbols.csv");
-
-    let file_create_mode = String::from("NEW");
-    dump_symbols_to_file(&output_base_location, &symbols_file_name, &file_create_mode, v);
-
-    let file_create_mode = String::from("APPEND");
-    dump_symbols_to_file(&output_base_location, &symbols_file_name, &file_create_mode, v2);
-}
-
-
-
-pub fn dump_symbols_to_file(file_location: &str, file_name: &str, create_mode: &str, symbols_vect:  Vec<std::ffi::OsString>) {
-    let file = concat_strings(&file_location,&file_name);
-    if create_mode == "NEW" {
-        create_file(file);
+    let mut f = fs::File::create("output.txt").expect("Unable to create file");
+    let data = get_names_of_symbol_files();
+    for i in  data{
+        //println!("{:?}", extract_symbol(i.symbol));
+        let mut symbol = extract_symbol(i.symbol);
+        f.write(symbol.as_bytes()).expect("Unable to write data");
+        f.write(b"\n").expect("Unable to write data");
     }
-    else if create_mode == "APPEND" {
-        append_file(file, symbols_vect);
-    }
-}
-
-pub fn append_file(file_loc_name:String, symbols_vect:  Vec<std::ffi::OsString>) {
-    //TODO
-    //let mut file = OpenOptions::new()
-    //    .write(true)
-    //    .append(true)
-    //    .open(file_loc_name)
-    //    .unwrap();
-
-    for i in symbols_vect {
-        let symbol = extract_symbol(i);
-        fs::write(file_loc_name, symbol).expect("Unable to write data");
-    }
-    //for i in v {
-    //    let symbol = extract_symbol(i);
-    //    println!("{}", symbol);
-    //}
-}
-
-pub fn create_file(file_loc_name:String) {
-    let path = Path::new(&file_loc_name);
-    let display = path.display();
-
-    let _file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
-        Ok(_file) => _file,
-    };
-
 }
 
 /// Returns a ticker symbol as a String
@@ -78,41 +25,30 @@ pub fn create_file(file_loc_name:String) {
 /// # Remarks
 ///
 /// This is a convenience function that extract the stock sumbol name
-pub fn extract_symbol(file_name: std::ffi::OsString) -> String{
-    let s:String = file_name.into_string().unwrap();
-    let dot_index = s.find('.').unwrap();
-    let symbol: String = s.chars().skip(0).take(dot_index).collect();
-    //println!("{}", symbol);
+pub fn extract_symbol(file_name: String) -> String{
+    let last_slash_index = file_name.rfind('/').unwrap();
+    let last_dot_index = file_name.rfind('.').unwrap();
+    let mut symbol: String = file_name.chars().skip(last_slash_index + 1).take(last_dot_index).collect();
+
+    let first_dot_index = symbol.find('.').unwrap();
+    symbol = symbol.chars().skip(0).take(first_dot_index).collect();
     return symbol
 }
 
-/// Returns vector of file names where each element is the name of the file
-///
-/// # Arguments
-///
-/// * `location` - folder location of the input files.
-///
-/// # Returns
-///
-/// * `v` - vector of file names.
-///
-/// # Remarks
-///
-/// This is a convenience function reads the contents of `location` and puts the file names into a vector
-pub fn get_names_of_symbol_files(location: &str) -> Vec<std::ffi::OsString>{
+pub fn get_names_of_symbol_files() -> Vec<Company> {
+    let paths = fs::read_dir("../input_data/Stocks/Test/").unwrap();
     let mut v = Vec::new();
-    if let Ok(entries) = fs::read_dir(location) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                // Here, `entry` is a `DirEntry`.
-                v.push(entry.file_name());
-            }
-        }
+    for path in paths {
+        let mut str_file_name: String = path.unwrap().path().to_str().unwrap().to_string();
+        let mut comp = Company {
+            symbol: str_file_name,
+        };
+        //println!("Name: {}", str_file_name);
+        v.push(comp);
     }
     return v
 }
 
-pub fn concat_strings(a: &str, b: &str) -> String{
-    let c = [a, b].join("");
-    return c
+pub struct Company {
+    symbol: String,
 }
