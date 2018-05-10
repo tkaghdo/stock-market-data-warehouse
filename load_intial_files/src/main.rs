1941c1a02e23f10df5d9aa42c8057bca6671c55e
Tamby Kaghdo <tamby.kaghdo@gmail.com>
2018-05-09 20:58:16 -0400

    next is id

--- load_intial_files/src/main.rs
+++ load_intial_files/src/main.rs
@@ -3,9 +3,18 @@ use std::io::Write;
 
 fn main() {
 
+    // grab all the ticker names from stokcs files
     let stocks_folder_location = "../input_data/Stocks/";
+    let symbol_file = "../output_data/stocks/symbols.csv";
+    create_symbols_file(&stocks_folder_location, &symbol_file);
 
-    let mut f = fs::File::create("output.txt").expect("Unable to create file");
+    // TODO: generate an ID for each symbol in the symbols file
+
+}
+
+pub fn create_symbols_file(stocks_folder_location: &str, symbol_file: &str) {
+
+    let mut f = fs::File::create(symbol_file).expect("Unable to create file");
     let data = get_names_of_symbol_files(&stocks_folder_location);
     for i in  data{
         //println!("{:?}", extract_symbol(i.symbol));
@@ -35,11 +44,12 @@ pub fn extract_symbol(file_name: String) -> String{
 
     let first_dot_index = symbol.find('.').unwrap();
     symbol = symbol.chars().skip(0).take(first_dot_index).collect();
+    symbol = symbol.to_uppercase();
     return symbol
 }
 
 pub fn get_names_of_symbol_files(folder_location: &str) -> Vec<Company> {
-    let paths = fs::read_dir("../input_data/Stocks/").unwrap();
+    let paths = fs::read_dir(folder_location).unwrap();
     let mut v = Vec::new();
     for path in paths {
         let mut str_file_name: String = path.unwrap().path().to_str().unwrap().to_string();
--- load_intial_files/target/debug/load_intial_files.d
+++ load_intial_files/target/debug/load_intial_files.d
@@ -1,1 +1,1 @@
-C:\Workspace\rust\stock-market-data-warehouse\load_intial_files\target\debug\load_intial_files.exe: C:\Workspace\rust\stock-market-data-warehouse\load_intial_files\src\main.rs
+/Users/tambykaghdo/workspace/stock-market-data-warehouse/load_intial_files/target/debug/load_intial_files.dSYM: /Users/tambykaghdo/workspace/stock-market-data-warehouse/load_intial_files/src/main.rs
